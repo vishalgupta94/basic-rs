7b080111a685a2c686fba4a61bebb485c17d23eb
Vishal <vishal.gupta@appfire.com>
2025-05-24 18:56:56 +0530

    basic rust

---
+++ target/debug/deps/basic_rs-2ddbb607ceb4ab36.d
@@ -0,0 +1,5 @@
+/Users/appfire/Docker/bbb/NNgrok/Art/BasicFargate/Photos-app/basic-rs/basic-rs/target/debug/deps/libbasic_rs-2ddbb607ceb4ab36.rmeta: src/main.rs
+
+/Users/appfire/Docker/bbb/NNgrok/Art/BasicFargate/Photos-app/basic-rs/basic-rs/target/debug/deps/basic_rs-2ddbb607ceb4ab36.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/basic_rs-3f55486fa1d9c881.d
@@ -0,0 +1,5 @@
+/Users/appfire/Docker/bbb/NNgrok/Art/BasicFargate/Photos-app/basic-rs/basic-rs/target/debug/deps/libbasic_rs-3f55486fa1d9c881.rmeta: src/main.rs
+
+/Users/appfire/Docker/bbb/NNgrok/Art/BasicFargate/Photos-app/basic-rs/basic-rs/target/debug/deps/basic_rs-3f55486fa1d9c881.d: src/main.rs
+
+src/main.rs:
